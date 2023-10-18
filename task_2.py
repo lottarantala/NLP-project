bd6543cdcaafdfc40ad9f897a1b9a201e7223640
Venla Timisjärvi <vtimisja20@student.oulu.fi>
2023-10-18 18:39:43 +0300

    Continuing task 2

--- task_2.py
+++ task_2.py
@@ -1,6 +1,12 @@
 import pandas as pd
+from task_1 import get_semantic
+import re
+import os
 
-#Read the ambiguous terms to a list:
+directory = "MSHCorpus/MSHCorpus/"
+
+##TURHAA?
+# Read the ambiguous terms to a list:
 
 with open("MSHCorpus/MSHCorpus/benchmark_mesh.txt", 'r') as file:
     ambiguous_terms = []
@@ -13,11 +19,44 @@ with open("MSHCorpus/MSHCorpus/benchmark_mesh.txt", 'r') as file:
         # Append the line to the list
         ambiguous_terms.append(values)
 
-# Print the list of ambiguous words and their CUIs. 
-print(ambiguous_terms)
-#print(len(ambiguous_terms))
+# Print the list of ambiguous words and their CUIs.
+# print(ambiguous_terms)
+# print(len(ambiguous_terms))
+##TURHA LOPPUU
+def find_file(word, directory):
+    for file in os.listdir(directory):
+        if file.startswith(word):
+            print(file)
+            return file
 
-#selecting 10 regular terms and 10 acronyms randomly
-regular_terms = ['Milk', 'Nursing', 'Glycoside', 'Borrelia', 'Crown', 'Crack', 'Ganglion', 'Lactation', 'Synapsis', 'veterinary' ]
+def load_arff_file(filename, directory):
+    data = []
+    with open(directory + "/" + filename, "r") as f:
+        for line in f:
+            line = line.replace('\n', '')
+            line = re.split(r',(?=")', line)
+            line = line[-1].rsplit(',', 1)
+            #print(line)
+            data.append(line)
+    names = ['citation string', 'class']
+    data = data[7:]
+    df = pd.DataFrame(data, columns=names)
+    return df
+
+def find_senses(word):
+    file = find_file(word, directory)
+    data = load_arff_file(file, directory)
+    #jatkuu...
+
+# selected 10 regular terms and 10 acronyms randomly
+regular_terms = ['Milk', 'Nursing', 'Glycoside', 'Borrelia', 'Crown',
+                 'Crack', 'Ganglion', 'Lactation', 'Synapsis', 'veterinary']
 acronyms = ['PVC', 'OCD', 'HIV', 'EGG', 'cRNA', 'AA', 'EM', 'MAF', 'PR', 'SPR']
 
+print("Regular terms:")
+for word in regular_terms:
+    find_senses(word)
+
+print("Acronyms:")
+for word in acronyms:
+    find_senses(word)
